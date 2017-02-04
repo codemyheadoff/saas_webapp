b3d1cae0f0c2761a111367ad98f3d1a01b2dd539
codemyheadoff <emmanuel.ohwojeheri@gmail.com>
2017-02-04 07:37:39 +0000

    added stripe payment

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,41 +1,59 @@
 $(document).ready(function() {
-    Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
     
-    //Watch for a form submission
-    $("#form-submit-btn").click(function(event) {
-        event.preventDefault();
-        $('input[type=submit]').prop('disable', true);
-        var error = false; 
-        var ccNum = $('#card_number').val(),
-            cvcNum = $('#card_code').val(),
-            expMonth = $('#card_month').val(),
-            expYear = $('#card_year').val();
-            
-            if (!error) {
-                // Get the stripe token
-                Stripe.createToken({
-                    number: ccNum,
-                    cvc: cvcNum,
-                    exp_month: expMonth,
-                    exp_year: expYear
-                }, stripeResponseHandler);
-            }
-            return false;
-            
-    }); //for submisiion 
-    
-    function stripeResponseHandler(status, response) {
-                // Get a reference to the form 
-                var f = $("#new_user");
-                
-                //Get the token from the response 
-                var token = response.id;
-                
-                // Add the token to the form:
-                f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-                
-                //Submit the form 
-                f.get(0).submit();
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
+        
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
     }
-         
+    return false;
+  }); // form submission
+    
+   function stripeResponseHandler(status, response) {
+  // Grab the form:
+  var f = $("#new_user");
+
+  /*if (response.error) { // Problem!
+
+    // Show the errors on the form:
+    f.find('.payment-errors').text(response.error.message);
+    f.find('.submit').prop('disabled', false); // Re-enable submission
+
+  } else { // Token was created!*/
+
+    // Get the token ID:
+    var token = response.id;
+    
+
+    // Insert the token ID into the form so it gets submitted to the server:
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '"/>');
+
+    // Submit the form:
+    f.get(0).submit();
+  }
 });
+
+
+
+
+
+
+
+
+
+
+
